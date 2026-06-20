7b40aaf0ce45f3635b9e5ea741cb8907baba3f25
chanduc591 <sekharc5916@gmail.com>
2026-06-20 09:24:19 +0530

    javascript basics

---
+++ javascript-training/language-basics/comments.js
@@ -0,0 +1,36 @@
+// There are two different types of comments we can add inside the JavaScript. 
+
+//1. Single line comment. => starts with //
+//2. Multi-line comments => starts with /* and ends with */
+
+
+
+// `console.log` is a print statement.  (single-line comment)  (Cntrl+/)
+
+/*
+(multi-line comments) (Cntrl+Shift+/)
+1. Education and Learning Purpose 
+2. Debugging and error detection. 
+3. Monitoring the program execution to understand the flow. 
+4. Testing and validating the results. 
+5. Log the system results to analyze later. 
+*/
+
+console.log("Hello, World!");
+
+
+/*
+ * This is a function that is going to help us to calculate the sum of two numbers. 
+ * @param {number} a - The first number to be added 
+ * @param {number} b - The second number to be added 
+ * @returns {number} - The sum of the two numbers a and b. 
+ * @author Bharath Reddy (BharathTechAcademy@gmail.com)
+ * @version 1.0
+ * @created 2026-06-16
+ */
+
+function add(a,b){
+    return a+b;
+}
+
+add(5,10); // 15
